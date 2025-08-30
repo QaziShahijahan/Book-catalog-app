65a69ab85d2912d196b5b145fb7da295abb1336c
mirfaizan10 <mirfaizaan.10@gmail.com>
2025-08-30 09:44:32 +0530

    middle ware changed

--- Routes/bookRoute.js
+++ Routes/bookRoute.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const { getBooks, getBook, createBook, updateBook, deleteBook } = require("../Controllers/bookController");
-const { protect } = require("../middleware/authMiddleware");
+const { protect } = require("../MiddleWare/authMiddleWare");
 
 const router = express.Router();
 
 
-router.get("/", getBooks);
-router.get("/:id", getBook);
-router.post("/", protect, createBook);
-router.put("/:id", protect, updateBook);
-router.delete("/:id", protect, deleteBook);
+router.get("/books", getBooks);
+router.get("/books/:id", getBook);
+router.post("/books", protect, createBook);
+router.put("/books/:id", protect, updateBook);
+router.delete("/books:id", protect, deleteBook);
 
 module.exports = router;
